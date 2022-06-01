0579e4a8d852b1fc06391fb33d6c17e07bdc88a6
Nicogamerman <gamermanico@gmail.com>
2022-05-31 22:44:09 -0300

    actualizacion diaria

--- abmclientes/index.php
+++ abmclientes/index.php
@@ -3,46 +3,43 @@
     ini_set('display_startup_errors',1);
     error_reporting(E_ALL);
 
-session_start(); 
 
-if(file_exists("archivo.txt")){ //si el archivo existe lo abrimos.
-    //si el archivo existe, cargo los clientes en la variable aClientes
-    $strJson = file_get_contents("archivo.txt"); //con file_get_contents busca y lee el archivo.txt
-    $aClientes = json_decode($strJson, true); //se coloca true para que sepa que se tiene que convertir en un array o no.
-}
+    if(file_exists("archivo.txt")){ //si el archivo existe lo abrimos.
+        //si el archivo existe, cargo los clientes en la variable aClientes
+        $strJson = file_get_contents("archivo.txt"); //con file_get_contents busca y lee el archivo.txt
+        $aClientes = json_decode($strJson, true); //se coloca true para que sepa que se tiene que convertir en un array o no.
+    }
 
-else {
+    else {
         //si el archivo no existe es porque no hay clientes
         $aClientes = array();
 
-}
+    }
 
-if ($_POST) //si la persona completo todos los datos viene toda esta info:
-
-    if($_POST){
-
-        $nombre = $_POST['txtNombre'];
-        $correo = $_POST['txtCorreo'];
-        $dni = $_POST['txtDni'];
-        $telefono = $_POST['txtTelefono'];
-
-        $aClientes = array("nombre" => $nombre,
+    //si la persona completo todos los datos viene toda esta info:
+     if($_POST){
+            $dni = $_POST["txtDni"];
+            $nombre = $_POST["txtNombre"]; 
+            $telefono = $_POST["txtTelefono"];
+            $correo = $_POST["txtCorreo"];
+            $nombreImagen = "";
+        
+        $aClientes [] = array("nombre" => $nombre, //$aClientes [] me agrega un nuevo elemento al array, que seria el nuevo cliente.
                         "correo" => $correo,
                         "dni" => $dni,
                         "telefono" => $telefono,
-                        );   
-        $_SESSION['listadoClientes'][] = $aClientes;      
-}
-
-else if(isset($_POST["btnNuevo"])){
-    session_unset(); 
-   } 
+        );   
+             
+    }
+// else if(isset($_POST["btnNuevo"])){
+//     session_unset(); 
+//    } 
 
 //Ahora tengo que convertir el array de cliente a JSON:
 //Almacenar en un archivo.txt el JSON
 
     $strJson = json_encode($aClientes);
-    file_put_contents("archivo.txt", $strJson)
+    file_put_contents("archivo.txt", $strJson);
 
     
 
@@ -70,19 +67,21 @@ else if(isset($_POST["btnNuevo"])){
 </head>
 <body>
     <main class="container">
-        <div class="row text-center py-5">
-            <h1>Registro de clientes ABM</h1>
-        </div>
+        <div class="row">
+            <div class="col-12 py-5 text-center">
+                <h1>Registro de clientes</h1>
+            </div>
+        </div>   
         <div class="row">
             <div class="col-6">                
                 <form action="" method="POST" class="form" enctype="multipart/form-data"> 
                     <!-- multipart/form.data sirve para adjuntar archivos, hay otros atributos para adjuntar texto o enviar datos como query string -->
 
-                    <label for="txtDni">DNI:</label>
-                    <input type="text" name="txtDni" class="form-control my-2">
-
                     <label for="txtNombre">Nombre:</label>
-                    <input type="text" name="txtNombre" class="form-control my-2">     
+                    <input type="text" name="txtNombre" class="form-control my-2">
+
+                    <label for="txtDni">Dni:</label>
+                    <input type="text" name="txtDni" class="form-control my-2">     
                     
                     <label for="txtTelefono">Telefono:</label>
                     <input type="text" name="txtTelefono" class="form-control my-2"> 
@@ -100,26 +99,27 @@ else if(isset($_POST["btnNuevo"])){
                        
                 </form> 
             </div>
-            <div class= "col-6">
-                <table class="table table-hover">
-                    <thead>
-                        <th>Imagen:</th>
-                        <th>DNI:</th>
-                        <th>Nombre:</th>
-                        <th>Correo:</th>                        
-                    </thead>
-                    <tbody>                    
-                        <?php
-                        foreach ($_SESSION["listadoClientes"] as $aClientes): ?> <!-- //el foreach recibe el array $listadoClientes , as $cliente que va a ser cada cliente individual. -->
+            <div class= "col-sm-6">
+                <table class="table table-hover border">
+                    <tr>
+                        <th>Imagen</th>
+                        <th>Nombre</th>
+                        <th>DNI</th>
+                        <th>Telefono</th>
+                        <th>Correo</th>   
+                        <th>Acciones</th>                     
+                    </tr>                                        
+                        <?php foreach ($aClientes as $cliente): ?> <!-- //el foreach recibe el array $aClientes , as $cliente que va a ser cada cliente individual. -->
                             <tr>
-                                <td> <?php echo $aClientes["dni"] ?> </td>
-                                <td> <?php echo $aClientes["nombre"] ?></td>
-                                <td> <?php echo $aClientes["telefono"] ?></td>
-                                <td> <?php echo $aClientes["correo"] ?></td>
-                                
+                                <td></td>
+                                <td> <?php echo $cliente["dni"]; ?> </td>
+                                <td> <?php echo $cliente["nombre"]; ?></td>
+                                <td> <?php echo $cliente["telefono"]; ?></td>
+                                <td> <?php echo $cliente["correo"]; ?></td>
+                                <td></td>
+                            
                             </tr>
-                        <?php endforeach; ?>
-                    </tbody>
+                        <?php endforeach; ?>                    
                 </table>
             </div>  
 
